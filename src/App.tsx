3c56846426b50c335b4c00787ffae0ce854477c4
tsotne01 <tsotnechkhenkeli@gmail.com>
2025-10-16 22:17:39 +0400

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import SignInPage from "./page/sign-in-page";
+import SignUpPage from "./page/sign-up-page";
+import Header from "./shared/ui/header";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SignInPage redirectTo="/dashboard" />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUpPage redirectTo="/dashboard" />,
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <div>
+        <Header />
+      </div>
+    ),
+  },
+]);
+
+const App = () => {
+  return (
+    <div className="bg-[#141414] text-white min-h-screen">
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { dark } from "@clerk/themes";
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ClerkProvider
+      appearance={{
+        theme: dark,
+      }}
+      publishableKey={PUBLISHABLE_KEY}
+    >
+      <App />
+    </ClerkProvider>
+  </StrictMode>
+);
